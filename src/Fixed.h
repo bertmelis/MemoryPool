1035c95649ee03e977bd5c695c5d5ec4b08929a4
Bert Melis <bertmelis@users.noreply.github.com>
2024-02-28 15:57:05 +0100

    spli var/fixed

---
+++ src/Fixed.h
@@ -0,0 +1,26 @@
+/*
+Copyright (c) 2024 Bert Melis. All rights reserved.
+
+This work is licensed under the terms of the MIT license.  
+For a copy, see <https://opensource.org/licenses/MIT> or
+the LICENSE file.
+*/
+
+#pragma once
+
+#include <cstddef>  // std::size_t
+#include <cassert>  // assert
+#include <mutex>  // NOLINT [build/c++11] std::mutex, std::lock_guard
+
+#ifdef MEMPOL_DEBUG
+#include <iostream>
+#endif
+
+namespace MemoryPool {
+
+template <std::size_t nrBlocks, std::size_t blocksize>
+class Fixed {
+  // to be implemented
+};
+
+}  // end namespace MemoryPool
