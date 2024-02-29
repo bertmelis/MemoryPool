6514006daa062f820382fff8be0313659e5bbc79
Bert Melis <bertmelis@users.noreply.github.com>
2024-02-29 11:10:33 +0100

    first try

--- src/Fixed.h
+++ src/Fixed.h
@@ -20,7 +20,80 @@ namespace MemoryPool {
 
 template <std::size_t nrBlocks, std::size_t blocksize>
 class Fixed {
-  // to be implemented
+ public:
+  Fixed()
+  : _buffer{0}
+  , _head(_buffer) {
+    unsigned char* b = _buffer;
+    std::size_t adjustedBlocksize = std::max(sizeof(void*), blocksize);
+    std::size_t i = blocksize - 1;
+    while (i) {
+      *b = b + adjustedBlocksize;
+      b += adjustedBlocksize;
+      --i;
+    }
+    *b = nullptr;
+  }
+
+  // no copy nor move
+  Fixed (const Fixed&) = delete;
+  Fixed& operator= (const Fixed&) = delete;
+
+  void* malloc() {
+    const std::lock_guard<std::mutex> lockGuard(_mutex);
+    if (_head) {
+      void* retVal = _head;
+      _head = *_head;
+      return retVal;
+    }
+    return nullptr;
+  }
+
+  void free(void* ptr) {
+    const std::lock_guard<std::mutex> lockGuard(_mutex);
+    *ptr = _head;
+    _head = ptr;
+  }
+
+  std::size_t freeMemory() {
+    const std::lock_guard<std::mutex> lockGuard(_mutex);
+    unsigned char* i = _head;
+    std::size_t retVal = 0;
+    while (i) {
+      retVal += blocksize;
+      i = *i;
+    }
+    return retVal;
+  }
+
+  #ifdef MEMPOL_DEBUG
+  void print() {
+    std::size_t adjBlocksize = std::max(sizeof(void*), blocksize);
+    std::cout << "+--------------------" << std::endl;
+    std::cout << "|start:" << static_cast<void*>(_buffer) << std::endl;
+    std::cout << "|blocks:" << nrBlocks << std::endl;
+    std::cout << "|blocksize:" << adjBlocksize << std::endl;
+    std::cout << "|head: " << static_cast<void*>(_head) << std::endl;
+    unsigned char* nextFreeBlock = _head;
+    unsigned char* currentBlock = _buffer;
+
+    for (std::size_t i = 0; i < nrBlocks; ++i) {
+      std::cout << "|" << i + 1 << ": " << static_cast<void*>(currentBlock) << std::endl;
+      if (currentBlock == nextFreeBlock) {
+        std::cout << "|   free" << std::endl;
+        nextFreeBlock = *currentBlock;
+        std::cout << "|   next: " << static_cast<void*>(nextFreeBlock) << std::endl;
+      } else {
+        std::cout << "|   allocated" << std::endl;
+      }
+    }    
+    std::cout << "+--------------------" << std::endl;
+  }
+  #endif
+
+ private:
+  unsigned char* _buffer[nrBlocks * std::max(sizeof(void*), blocksize)];
+  unsigned char* _head;
 };
 
 }  // end namespace MemoryPool
