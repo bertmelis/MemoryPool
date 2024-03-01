be96e10c3390c17537d4967c6b913fafb4656cd0
Bert Melis <bertmelis@users.noreply.github.com>
2024-03-01 10:42:00 +0100

    add test full

--- test/test_FixInt/test_FixInt.cpp
+++ test/test_FixInt/test_FixInt.cpp
@@ -25,8 +25,35 @@ void emptyPool() {
   TEST_ASSERT_EQUAL_UINT(nrBlocks * adjustedBlocksize, pool.freeMemory());
 }
 
+void mallocFull() {
+  const size_t nrBlocks = 3;
+  const size_t blocksize = sizeof(int);
+  MemoryPool::Fixed<nrBlocks, blocksize> pool;
+
+  int* int1 = reinterpret_cast<int*>(pool.malloc());
+  int* int2 = reinterpret_cast<int*>(pool.malloc());
+  int* int3 = reinterpret_cast<int*>(pool.malloc());
+  int* int4 = reinterpret_cast<int*>(pool.malloc());
+
+  pool.print();
+
+  TEST_ASSERT_NOT_NULL(int1);
+  TEST_ASSERT_NOT_NULL(int2);
+  TEST_ASSERT_NOT_NULL(int3);
+  TEST_ASSERT_NULL(int4);
+
+  *int1 = 1;
+  *int2 = 2;
+  *int3 = 3;
+  TEST_ASSERT_EQUAL_INT(1, *int1);
+  TEST_ASSERT_EQUAL_INT(2, *int2);
+  TEST_ASSERT_EQUAL_INT(3, *int3);
+  TEST_ASSERT_EQUAL_UINT(0, pool.freeMemory());
+}
+
 int main() {
   UNITY_BEGIN();
   RUN_TEST(emptyPool);
+  RUN_TEST(mallocFull)
   return UNITY_END();
 }
