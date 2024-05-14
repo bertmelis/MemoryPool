49a7754086d050cda80548ea5a7c03d6c42066c2
Bert Melis <bertmelis@users.noreply.github.com>
2024-05-14 16:30:26 +0200

    fix cppcheck scanning

---
+++ src/MemoryPool.cpp
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2024 Bert Melis. All rights reserved.
+
+This work is licensed under the terms of the MIT license.  
+For a copy, see <https://opensource.org/licenses/MIT> or
+the LICENSE file.
+*/
+
+#pragma once
+
+#include "MemoryPool.h"
+
+// dummy file to enable cppcheck
